b372e8337f6316fff97d77d49fe6a40249ad3ca1
nyu <nyu@debian.dsa.com.cn>
2018-01-31 17:22:45 +0800

    Refactor the sources.

--- test.js
+++ test.js
@@ -1,42 +1,47 @@
 var aliveSession = window.setInterval("keepMeAlive('ctl00_imgMasterPic')", 60000);
 
-//search();
+//the year month and date are scheduled training date.
+//The h, m, sec, are today's commit time.
+//booking on year, month(start at 0), date. Committed at h, m, sec, today.
+var d = new Date(2018, 2-1, 7, 14, 52, 50);
 
-var autoSubmit = window.setInterval("search()", 1000);
+var autoSubmit = window.setInterval(function() { booking(d, 12, 13, '9112024719'); }, 1000);
 
-function search() {
-	//setInterval(function(){
-	setDate(2, 4, 9016001067);
-	//}, 30000);
-}
-
-function setDate(month, day, coachID) {
-	if (document.referrer.match(/.*BookingCWStudy.*/)) {
-		clearInterval(aliveSession);
-		clearInterval(autoSubmit);
-		console.log(' Already booked skipping ...');
-	} else {
-		date = new Date(); 
-		if ((date.getHours() != 23) || (date.getMinutes() != 59) || (date.getSeconds() != 57)) {
-			console.log('Probing coach: ' + coachID + ' On: ' + month + '-' + day + ' At: ' + date);
-		} else {
-			var my = window.open("http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName="+coachID+"&date=2018-"+month+"-"+day+"&beginTime=1400&trainType=cw&timeLine=15", "_blank"); 
-			var my = window.open("http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName="+coachID+"&date=2018-"+month+"-"+day+"&beginTime=1500&trainType=cw&timeLine=16", "_blank"); 
-			clearInterval(aliveSession);
-			clearInterval(autoSubmit);
-			console.log(' Booking finished !!!!!!! ');
-			//location.href="javascript:window.open('http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName=9112024719&date=2018-"+month+"-"+day+"&beginTime=1500&trainType=cw&timeLine=16', '_blank'); void 0";
-			//var my = window.open("http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName=9112024719&date=2018-"+month+"-"+day+"&beginTime=1500&trainType=cw&timeLine=16", "_blank"); 
-			//						//		console.log(my);
-			//									//			setTimeout(autoClick, 2000);
-			//												//
-			//
-		}
-	}
+/**
+ * booking @coachID at @date, between @startHour to @endHour.
+ */
+function booking(date, startHour, endHour, coachID) {
+    if (document.referrer.match(/.*BookingCWStudy.*/)) {
+        clearInterval(aliveSession);
+        clearInterval(autoSubmit);
+        console.log(' Already booked skipping ...');
+    } else {
+        nowDate = new Date(); 
+        var startHourEnd = startHour + 1;
+        var endHourEnd = endHour + 1;
+        var month = date.getMonth() + 1;
+        var dayofMonth = date.getDate();
+        if ((date.getHours() != nowDate.getHours()) || (date.getMinutes() != nowDate.getMinutes()) || (date.getSeconds() != nowDate.getSeconds())) {
+            console.log('Booking date: ' + date);
+            console.log('Probing coach: ' + coachID + ' On: ' + month + '-' + dayofMonth + ' At: ' + nowDate);
+        } else {
+            var my = window.open("http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName=" + coachID + "&date=" + date.getFullYear() + "-" + month + "-" + dayofMonth + "&beginTime=" + startHour + "00&trainType=cw&timeLine=" + startHourEnd , "_blank"); 
+            var my = window.open("http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName=" + coachID + "&date=" + date.getFullYear() + "-" + month + "-" + dayofMonth + "&beginTime=" + endHour + "00&trainType=cw&timeLine=" + endHourEnd, "_blank"); 
+            clearInterval(aliveSession);
+            clearInterval(autoSubmit);
+            console.log(' Booking finished !!!!!!! ');
+            //location.href="javascript:window.open('http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName=9112024719&date=2018-"+month+"-"+day+"&beginTime=1500&trainType=cw&timeLine=16', '_blank'); void 0";
+            //var my = window.open("http://117.74.136.117:8087/PupilWeb/logging/BookingCWStudy.aspx?coachName=9112024719&date=2018-"+month+"-"+day+"&beginTime=1500&trainType=cw&timeLine=16", "_blank"); 
+            //						//		console.log(my);
+            //									//			setTimeout(autoClick, 2000);
+            //												//
+            //
+        }
+    }
 }
 
 function keepMeAlive(imgName) {
-	myImg = document.getElementById(imgName);
-	if (myImg) myImg.src = myImg.src.replace(/.*$/, '?' + Math.random());
-	console.log(myImg.src);
+    myImg = document.getElementById(imgName);
+    if (myImg) myImg.src = myImg.src.replace(/.*$/, '?' + Math.random());
+    console.log(myImg.src);
 }
